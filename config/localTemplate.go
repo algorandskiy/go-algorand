696f0e88b68da401b3b92d841bab9f8ff4809419
chris erway <chris.erway@algorand.com>
2023-02-01 08:30:39 -0500

    simpler REST soft limit ceil calc

--- config/localTemplate.go
+++ config/localTemplate.go
@@ -606,12 +606,6 @@ func (cfg *Local) AdjustConnectionLimits(requiredFDs, maxFDs uint64) bool {
 	const reservedRESTConns = 10
 	diff := requiredFDs - maxFDs
 
-	if cfg.RestConnectionsSoftLimit <= diff+reservedRESTConns {
-		cfg.RestConnectionsSoftLimit = reservedRESTConns
-	} else {
-		cfg.RestConnectionsSoftLimit -= diff
-	}
-
 	if cfg.RestConnectionsHardLimit <= diff+2*reservedRESTConns {
 		restDelta := diff + 2*reservedRESTConns - cfg.RestConnectionsHardLimit
 		cfg.RestConnectionsHardLimit = 2 * reservedRESTConns
@@ -622,6 +616,9 @@ func (cfg *Local) AdjustConnectionLimits(requiredFDs, maxFDs uint64) bool {
 		}
 	} else {
 		cfg.RestConnectionsHardLimit -= diff
+	}
+	if cfg.RestConnectionsSoftLimit > cfg.RestConnectionsHardLimit {
+		cfg.RestConnectionsSoftLimit = cfg.RestConnectionsHardLimit
 	}
 	return true
 }
