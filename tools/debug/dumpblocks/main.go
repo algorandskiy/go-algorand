9d7f081353827f187ac4bc44b1a1026cec590987
Pavel Zbitskiy <pavel@algorand.com>
2023-04-10 19:29:26 -0400

    debug: dump blocks with expiration

--- tools/debug/dumpblocks/main.go
+++ tools/debug/dumpblocks/main.go
@@ -25,6 +25,9 @@ import (
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/algorand/go-algorand/data/bookkeeping"
+	"github.com/algorand/go-algorand/protocol"
 )
 
 var blockDBfile = flag.String("blockdb", "", "Block DB filename")
@@ -48,13 +51,35 @@ func usage() {
 	os.Exit(1)
 }
 
+func dumpBlocks(db *sql.DB, rnd, spLookback, deltas int64) {
+	var buf []byte
+	err := db.QueryRow("SELECT hdrdata FROM blocks WHERE rnd=?", rnd).Scan(&buf)
+	if err != nil {
+		fmt.Println("Error getting block", rnd, err)
+		return
+	}
+	var hdr bookkeeping.BlockHeader
+	err = protocol.Decode(buf, &hdr)
+	if err != nil {
+		fmt.Println("Error decoding block", rnd, err)
+		return
+	}
+	var votersRnd = rnd + spLookback + deltas
+	if hdr.ParticipationUpdates.ExpiredParticipationAccounts != nil {
+		fmt.Printf("expired at rnd %d = %d - (%d + %d)\n", rnd, votersRnd, spLookback, deltas)
+		for _, addr := range hdr.ParticipationUpdates.ExpiredParticipationAccounts {
+			fmt.Printf("%s\n", addr.String())
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	if *blockDBfile == "" {
 		fmt.Println("-blockdb=file required")
 		usage()
 	}
-	uri := fmt.Sprintf("file:%s?mode=ro", *blockDBfile)
+	uri := fmt.Sprintf("file:%s?_journal_mode=wal", *blockDBfile)
 	fmt.Println("Opening", uri)
 	db, err := sql.Open("sqlite3", uri)
 	if err != nil {
@@ -93,30 +118,15 @@ func main() {
 		}
 	}
 
-	N := maxRound - minRound
-	if N <= 0 {
-		panic("maxRound must be greater than minRound")
-	}
-
-	if N <= int64(*numBlocks) {
-		// just get all blocks from minRound to maxRound
-		fmt.Printf("Saving all blocks between round %d and %d\n", minRound, maxRound)
-		for i := minRound; i <= maxRound; i++ {
-			err = getBlockToFile(db, i)
-			if err != nil {
-				panic(err)
+	// just analyze all blocks from minRound to maxRound
+	const spLookback = 16
+	const maxDelta = 16
+	fmt.Printf("Processing all blocks between round %d and %d\n", minRound, maxRound)
+	for i := minRound; i <= maxRound; i++ {
+		for d := int64(0); d <= maxDelta; d++ {
+			if (i+spLookback+d)%256 == 0 {
+				dumpBlocks(db, i, spLookback, d)
 			}
-
-		}
-		os.Exit(0)
-	}
-
-	fmt.Printf("Loading %d random blocks between round %d and %d\n", *numBlocks, minRound, maxRound)
-	for i := 0; i < *numBlocks; i++ {
-		round := minRound + rand.Int63n(N) + 1
-		err = getBlockToFile(db, round)
-		if err != nil {
-			panic(err)
 		}
 	}
 }
